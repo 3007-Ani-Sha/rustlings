1950cc798dd6ac636e1f1a8e8bdcae0c1117e70b
Aniket <sharmaa20@iitk.ac.in>
2021-06-09 09:12:08 +0530

    completed exercise primitive_types with 4th not running

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,10 +5,17 @@
 // I AM NOT DONE
 
 #[test]
-fn slice_out_of_array() {
+fn slice_out_of_array()->bool {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..3];
 
     assert_eq!([2, 3, 4], nice_slice)
+}
+fn main(){
+    if slice_out_of_array() {
+        println!("true");
+    }else{
+        println!("false"); 
+    }
 }
